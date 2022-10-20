583673522e8495f4a1d54eec72041e2955b74926
Andrea Burr <burrandy18@gmail.com>
2022-10-20 13:48:21 -0700

    update push

--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
 ﻿// Andrea Burr
-// 10-18-2022
+// 10-20-2022
 // Reverse it 
 using System;
 
-
-    //class Program
-//     {
-
-// static void Main(string[] args)
-// {
+Console.Clear();
+bool playagain = true;
+while (playagain){
 Console.WriteLine("Hello Player lets play reverse it ");
-//string Num = Console.ReadLine();
-// string reverseNum = string.reverse(Num);
-// Console.WriteLine(Num + "written backwards" + reverseNum);
-// }
+
 
 int[] intarray = { 1, 2 ,3, 4, 5};
-//Console.WriteLine(intarray.Length);
+
 Console.WriteLine(intarray [4] );
 for(int  i = intarray.Length -1 ; i > 0; i-- ){
     Console.WriteLine(i);
+Console.WriteLine("if you are done playing type no or press any key to continue");
+string contPlay = Console.ReadLine();
 
+  if (contPlay == "no"){
+     playagain = false; 
+  }else{
+    playagain = true;
+  }
 }
+    }
