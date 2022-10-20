adb2300230d800d7debefe0bc6082bbf4705d6cd
Andrea Burr <burrandy18@gmail.com>
2022-10-20 11:19:33 -0700

    ReverseMini-Git

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Andrea Burr
+// 10-18-2022
+// Reverse it 
+using System;
+
+
+    //class Program
+//     {
+
+// static void Main(string[] args)
+// {
+Console.WriteLine("Hello Player lets play reverse it ");
+//string Num = Console.ReadLine();
+// string reverseNum = string.reverse(Num);
+// Console.WriteLine(Num + "written backwards" + reverseNum);
+// }
+
+int[] intarray = { 1, 2 ,3, 4, 5};
+//Console.WriteLine(intarray.Length);
+Console.WriteLine(intarray [4] );
+for(int  i = intarray.Length -1 ; i > 0; i-- ){
+    Console.WriteLine(i);
+
+}
